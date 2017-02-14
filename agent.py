24a8e1ad811aa267f394de56839cfa961f88b1be
sheh1000 <d.shelikhov@gmail.com>
2017-02-14 08:14:45 +0000

    added method to put custom events

--- agent.py
+++ agent.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 import sys
 import os
 import requests
@@ -7,6 +5,7 @@ import socket
 import json
 import logging
 import time
+from poaupdater import uLogging
 
 #--------------------------------------------------------------------
 class Newrelic_Metrics(object):
@@ -26,33 +25,37 @@ class Newrelic_Metrics(object):
     def http_request(self, url, proxies, headers, data, timeout):
         """ HTTP request """
         try:
-            logger.info("Sending HTTP POST request: %s " % (url), extra={'hostname': hostname})
+            uLogging.info("Sending HTTP POST request: %s " % (url))
             encoded_str = json.dumps(data)
             start_timer = time.time()
-
-            if proxies:
-                r = requests.post(url,
-                                proxies=proxies,
+            print("encoded_str = {0}".format(encoded_str))
+            if self.proxies:
+                r = requests.post(url=url,
+                                proxies=self.proxies,
                                 headers=headers, 
                                 data=encoded_str, 
                                 timeout=timeout)
             else:
-                r = requests.post(url, headers=headers, data=encoded_str, timeout=timeout)
+                r = requests.post(url=url, headers=headers, data=encoded_str, timeout=timeout)
 
             response_time = "%.2f" % (time.time() - start_timer)
             if r.status_code == requests.codes.ok:
-                logger.info("HTML code: %s" % (r.status_code), extra={'hostname': hostname})
+                uLogging.info("HTML code: %s" % (r.status_code))
                 return True, r.status_code, r.headers, response_time
             else:
-                logger.info("\nAn error occurred, status code %s" % (r.status_code), extra={'hostname': hostname})
+                uLogging.info("\nAn error occurred, status code %s" % (r.status_code))
                 return False, r.status_code, r.headers, response_time
         except requests.exceptions.RequestException, e:
-            logger.info('\nERROR: Request exception occurred', extra={'hostname': hostname})
-            logger.info(e, extra={'hostname': hostname})
+            uLogging.info('\nERROR: Request exception occurred')
+            uLogging.info(e)
             return False, None, None, None, None
 
-    def get_payload(self):
-        """ Returns formatted payload """
+
+    def newrelic_post_request(self):
+        # disable url urllib3 warnings for older python versions
+        requests.packages.urllib3.disable_warnings()
+
+        """ Getting formatted payload """
         agent={}
         agent["host"]=self.hostname
         agent["version"]=self.agent_version
@@ -68,53 +71,31 @@ class Newrelic_Metrics(object):
         payload["components"]=components
 
         for key, value in self.metrics.iteritems():
-            logger.info("%s %s" % (key, value), extra={'hostname': hostname})
-
-        return payload
-
-    def newrelic_post_request(self):
-        # disable url urllib3 warnings for older python versions
-        requests.packages.urllib3.disable_warnings()
+            uLogging.info("%s %s" % (key, value))
         
-        payload = self.get_payload()
-        logger.info("Payload: \n%s" % payload, extra={'hostname': hostname})
+        uLogging.info("Payload: \n%s" % payload)
         headers = {"Content-Type": "application/json", 
                    "Accept": "application/json", 
                    "X-License-Key": self.nr_licence_key}
-        logger.info("headers: \n%s" % headers, extra={'hostname': hostname})
+        uLogging.info("headers: \n%s" % headers)
         (request_status, status_code, headers, response_time) = self.http_request(self.nr_api_url, self.proxies, headers, payload, 10)
-        logger.info("HTTP request status:\n %s, \n HTTP code:%s, \n Headers: %s, \n Latency:%s\n\n" % (request_status, 
+        uLogging.info("HTTP request status:\n %s, \n HTTP code:%s, \n Headers: %s, \n Latency:%s\n\n" % (request_status, 
                                                                                                                  status_code, 
                                                                                                                  headers, 
-                                                                                                                 response_time), 
-                                                                                                                 extra={'hostname': hostname})
+                                                                                                                 response_time))
+    def insights_post_request(self, xinsertkey, account_id):
+        requests.packages.urllib3.disable_warnings()
+        url = 'https://insights-collector.newrelic.com/v1/accounts/{0}/events'.format(account_id)
+        payload = self.metrics
+        uLogging.info("Payload: \n%s" % payload)
+        headers = {"Content-Type": "application/json",  
+                   "X-Insert-Key": xinsertkey}
+        uLogging.info("headers: \n%s" % headers)
+        (request_status, status_code, headers, response_time) = self.http_request(url, self.proxies, headers, payload, 10)
+        uLogging.info("HTTP request status:\n %s, \n HTTP code:%s, \n Headers: %s, \n Latency:%s\n\n" % (request_status, 
+                                                                                                                 status_code, 
+                                                                                                                 headers, 
+                                                                                                                 response_time))
 
 
-if __name__ == "__main__":
-    """ Configure logging settings """ 
-    logging.basicConfig(format='%(asctime)s %(message)s', filename='/var/log/newrelic_plugin.log', level=logging.INFO)
-    logger = logging.getLogger(__name__)
 
-    """ Take arguments from Robot Framework test """ 
-    domain = sys.argv[1]            # cp domain name
-    val = sys.argv[2]               # test time. Will be "-1" if failed
-    nr_licence_key = sys.argv[3]    # customer New Relic license key
-
-    """  Prepare data before sending to New Relic plugin API """ 
-    hostname = socket.gethostname()
-    metrics = {}
-    metrics["Custom/ControlPanel/%s" % domain]=float(val)
-    newrelic_utils = Newrelic_Metrics(
-                        proxies={
-                                "https":"https://<proxy_ip>:3128"
-                                },
-                        #proxies=False,
-                        nr_licence_key=nr_licence_key, 
-                        agent_name="OdinRobotFramework", 
-                        agent_version="1.0.0", 
-                        guid="OdinMonitoringPlugin", 
-                        poll_cycle=60, 
-                        metrics=metrics, 
-                        hostname=hostname)
-
-    newrelic_utils.newrelic_post_request()
